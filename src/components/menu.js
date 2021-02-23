68646b9a24546a63e2fb7366445ebb6659382f94
Shoji Makino <pitang1965@gmail.com>
2021-02-23 11:22:43 +0900

    Sort WP pages by menuOrder, etc.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,35 +1,34 @@
-import React from "react"
-import { graphql, useStaticQuery, Link } from "gatsby"
-import { slide as Animation } from "react-burger-menu"
+import React from 'react';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import { slide as Animation } from 'react-burger-menu';
 
 const Menu = () => {
   const data = useStaticQuery(graphql`
     {
-      allWpPage {
+      allWpPage(sort: { fields: [menuOrder], order: ASC }) {
         edges {
           node {
             status
             title
             uri
+            menuOrder
           }
         }
       }
     }
-  `)
+  `);
 
   return (
-    <Animation width={460}>
-      {data.allWpPage.edges.map(edge => {
-        if (edge.node.status === "publish") {
-          return (
-            <Link to={edge.node.uri} className="menu-item" key={edge.node.uri}>
-              {edge.node.title}
-            </Link>
-          )
-        }
-      })}
+    <Animation width={470}>
+      {data.allWpPage.edges.map(edge =>
+        edge.node.status === 'publish' ? (
+          <Link to={edge.node.uri} className="menu-item" key={edge.node.uri}>
+            {edge.node.title}
+          </Link>
+        ) : null
+      )}
     </Animation>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
