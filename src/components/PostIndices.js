74181823da2a6ba84a633864501755b84a1ffcd4
Shoji Makino <pitang1965@gmail.com>
2021-03-11 20:51:24 +0900

    Refactoring by adding src\components\PostIndices.js

---
+++ src/components/PostIndices.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Image from 'gatsby-image';
+import parse from 'html-react-parser';
+
+const PostIndices = ({ posts }) => (
+  <ol style={{ listStyle: `none` }}>
+    {posts.map(post => {
+      const title = post.title;
+      const featuredImage = {
+        fixed: post.featuredImage?.node?.localFile?.childImageSharp?.fixed,
+        alt: post.featuredImage?.node?.altText || ``,
+      };
+
+      return (
+        <li key={post.uri}>
+          <article
+            className="post-list-item"
+            itemScope
+            itemType="http://schema.org/Article"
+          >
+            <header className="post-header">
+              <h2>
+                <Link to={post.uri} itemProp="url">
+                  <span itemProp="headline">{parse(title)}</span>
+                </Link>
+              </h2>
+              <small>{post.date}</small>
+            </header>
+            {featuredImage?.fixed && (
+              <Image
+                className="post-image"
+                fixed={featuredImage.fixed}
+                alt={featuredImage.alt}
+                style={{ marginBottom: 50 }}
+              />
+            )}
+            <section className="post-excerpt" itemProp="description">
+              {parse(post.excerpt)}
+            </section>
+          </article>
+        </li>
+      );
+    })}
+  </ol>
+);
+
+export default PostIndices;
--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
-import Image from 'gatsby-image';
-import parse from 'html-react-parser';
-
+import { graphql } from 'gatsby';
 import Bio from '../components/bio';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import PostIndices from '../components/PostIndices';
 import { Button, ButtonsWrapper } from '../components/Button';
 
 const BlogIndex = ({
@@ -30,49 +28,8 @@ const BlogIndex = ({
   return (
     <Layout isHomePage>
       <SEO title="全投稿" />
-
       <Bio />
-
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.title;
-          const featuredImage = {
-            fixed: post.featuredImage?.node?.localFile?.childImageSharp?.fixed,
-            alt: post.featuredImage?.node?.altText || ``,
-          };
-
-          return (
-            <li key={post.uri}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header className="post-header">
-                  <h2>
-                    <Link to={post.uri} itemProp="url">
-                      <span itemProp="headline">{parse(title)}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.date}</small>
-                </header>
-                {featuredImage?.fixed && (
-                  <Image
-                    className="post-image"
-                    fixed={featuredImage.fixed}
-                    alt={featuredImage.alt}
-                    style={{ marginBottom: 50 }}
-                  />
-                )}
-                <section className="post-excerpt" itemProp="description">
-                  {parse(post.excerpt)}
-                </section>
-              </article>
-            </li>
-          );
-        })}
-      </ol>
-
+      <PostIndices posts={posts}/>
       <ButtonsWrapper>
         {previousPagePath && (
           <>
