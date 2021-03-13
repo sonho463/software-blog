68e2564be55183074db0cec338aa382da1a5527d
Shoji Makino <pitang1965@gmail.com>
2021-03-13 12:09:23 +0900

    Modify CSS of Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,45 @@ import { FaTwitter, FaInstagram, FaLink } from 'react-icons/fa';
 import { IconLink } from './IconLink';
 import { GlobalStyles } from 'twin.macro';
 
+const LayoutWrapper = styled.div`
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  padding: var(--spacing-10) var(--spacing-5);
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+`;
+
+const GlobalHeader = styled.header`
+  margin-bottom: var(--spacing-12);
+`;
+
+const MainHeading = styled.div`
+  font-size: var(--fontSize-4);
+  margin: 0;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: var(--fontSize-7);
+  }
+`;
+
+const HeaderLinkHome = styled.div`
+  display: flex;
+  justify-content: center;
+  font-weight: var(--fontWeight-bold);
+  font-family: var(--font-heading);
+  text-decoration: none;
+  font-size: var(--fontSize-4);
+`;
+
 const FooterWrapper = styled.footer`
   display: flex;
   align-items: center;
@@ -45,18 +84,18 @@ const Layout = ({ isHomePage, children }) => {
     <>
       <GlobalStyles />
       <Menu width={410} />
-      <div className="global-wrapper" data-is-root-path={isHomePage}>
-        <header className="global-header">
+      <LayoutWrapper>
+        <GlobalHeader>
           {isHomePage ? (
-            <h1 className="main-heading">
+            <MainHeading>
               <Link to="/">{parse(title)}</Link>
-            </h1>
+            </MainHeading>
           ) : (
-            <Link className="header-link-home" to="/">
-              {title}
-            </Link>
+            <HeaderLinkHome>
+              <Link to="/">{title}</Link>
+            </HeaderLinkHome>
           )}
-        </header>
+        </GlobalHeader>
         <main>{children}</main>
         <TagCloud />
         <FooterWrapper>
@@ -82,7 +121,7 @@ const Layout = ({ isHomePage, children }) => {
             label="Linktree"
           />
         </FooterWrapper>
-      </div>
+      </LayoutWrapper>
     </>
   );
 };
